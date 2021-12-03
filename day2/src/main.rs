100e19db9dc3e1895e0317e3104e35eddf080f42
shupei <freddiechang1994@live.com>
2021-12-03 13:56:02 -0700

    better day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,19 +4,21 @@ fn main() {
     let filename = String::from("input.txt");
     let contents: String = fs::read_to_string(filename).unwrap();
     let contents_split: Vec<&str> = contents.split('\n').collect();
+    let f = |x: &str| x.parse::<i32>().unwrap();
     let mut h: i32 = 0;
     let mut d: i32 = 0;
     // part 1
     for i in contents_split.iter() {
         let tokens: Vec<&str> = i.split(' ').collect();
+        let n = f(tokens[1]);
         if tokens[0].eq("forward") {
-            h += tokens[1].parse::<i32>().unwrap();
+            h += n;
         }
         else if tokens[0].eq("down") {
-            d += tokens[1].parse::<i32>().unwrap();
+            d += n;
         }
         else {
-            d -= tokens[1].parse::<i32>().unwrap();
+            d -= n;
         }
     }
     println!("{}", h * d);
@@ -26,17 +28,17 @@ fn main() {
     let mut aim: i32 = 0;
     for i in contents_split.iter() {
         let tokens: Vec<&str> = i.split(' ').collect();
+        let n = f(tokens[1]);
         if tokens[0].eq("forward") {
-            let f = tokens[1].parse::<i32>().unwrap();
-            h += f;
-            d += aim * f;
+            h += n;
+            d += aim * n;
 
         }
         else if tokens[0].eq("down") {
-            aim += tokens[1].parse::<i32>().unwrap();
+            aim += n;
         }
         else {
-            aim -= tokens[1].parse::<i32>().unwrap();
+            aim -= n;
         }
     }
     println!("{}", h * d);
