b18004bd7bed974211641431eb80c0819b79e6cd
shupei <freddiechang1994@live.com>
2021-12-03 13:50:12 -0700

    better day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,23 +3,23 @@ use std::fs;
 fn main() {
     let filename = String::from("input.txt");
     let contents: String = fs::read_to_string(filename).unwrap();
-    let contents_split: Vec<&str> = contents.split('\n').collect();
+    let f = |x: &str| x.parse::<i32>().unwrap();
+    let nums: Vec<i32> = contents.split('\n').map(f).collect();
     // Part 1
     let mut count: usize = 0;
     let mut last: i32 = i32::max_value();
-    for i in contents_split.iter() {
-        let num = i.parse::<i32>().unwrap();
-        if  num > last { count += 1; }
-        last = num;
+    for i in nums.iter() {
+        if  *i > last { count += 1; }
+        last = *i;
     }
     println!("{}", count);
     // Part 2
     let mut count: usize = 0;
     let mut last: i32 = i32::max_value();
-    for i in 0..=(contents_split.len() - 3) {
-        let num = contents_split[i].parse::<i32>().unwrap() + 
-            contents_split[i + 1].parse::<i32>().unwrap() +
-            contents_split[i + 2].parse::<i32>().unwrap();
+    for i in 0..=(nums.len() - 3) {
+        let num = nums[i] + 
+            nums[i + 1] +
+            nums[i + 2];
         if  num > last { count += 1; }
         last = num;
     }
