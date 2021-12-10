27ece03802f80a609ce12940bae7389d868c5463
shupei <freddiechang1994@live.com>
2021-12-10 15:49:10 -0700

    day9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -15,7 +15,32 @@ fn part_1(heights: &Vec<Vec<i32>>) -> i32 {
 }
 
 fn search_basin(heights: &Vec<Vec<i32>>, visited: &mut Vec<Vec<bool>>, i: usize, j: usize) -> i32 {
-    3
+    let mut sum = 0;
+    if !visited[i][j] && heights[i][j] != 9 {
+        sum += 1;
+        visited[i][j] = true;
+        if i != 0 {
+            if heights[i][j] < heights[i - 1][j] {
+                sum += search_basin(heights, visited, i - 1, j);
+            }
+        }
+        if i != heights.len() - 1 {
+            if heights[i][j] < heights[i + 1][j] {
+                sum += search_basin(heights, visited, i + 1, j);
+            }
+        }
+        if j != 0 {
+            if heights[i][j] < heights[i][j - 1] {
+                sum += search_basin(heights, visited, i, j - 1);
+            }
+        }
+        if j != heights[0].len() - 1 {
+            if heights[i][j] < heights[i][j + 1] {
+                sum += search_basin(heights, visited, i, j + 1);
+            }
+        }
+    }
+    sum
 }
 
 
@@ -28,7 +53,7 @@ fn part_2(heights: &Vec<Vec<i32>>) -> i32 {
             if i != heights.len() - 1 { if heights[i][j] >= heights[i + 1][j] { continue; } }
             if j != 0 { if heights[i][j] >= heights[i][j - 1] { continue; } }
             if j != heights[0].len() - 1 { if heights[i][j] >= heights[i][j + 1] { continue; } }
-            let temp = search_basin(heights, &mut visited);
+            let temp = search_basin(heights, &mut visited, i, j);
             for (idx, k) in biggest.iter().enumerate() {
                 if temp >= *k {
                     biggest.insert(idx, temp);
@@ -38,7 +63,7 @@ fn part_2(heights: &Vec<Vec<i32>>) -> i32 {
             }
         }
     }
-    sum
+    biggest[0] * biggest[1] * biggest[2]
 }
 
 
@@ -55,4 +80,5 @@ fn main() {
     }
     
     println!("{}", part_1(&heights));
+    println!("{}", part_2(&heights));
 }
